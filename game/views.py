656307d585d5880263b2b245bedf22e9cd03c0ed
Zseselja <zseselja@uvic.ca>
2016-01-07 20:08:07 -0800

    created game/views.py & edited ./urls.py

--- game/views.py
+++ game/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.conf.urls import url
+from datastore import models
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the game index.")
+
+
--- swiftcurrent/urls.py
+++ swiftcurrent/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from game import views as gameviews
+
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'data/', include('datastore.urls')),
+    url(r'game/', gameviews.index , name='game_index')
 ]
